89117f2bfe6ccc023bdbac843447b8067d875f42
rfloarea <account@rfloarea.net>
2024-03-26 12:46:23 -0400

    simple GUI with connected script

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function searchWeather() {
-    const city = prompt("Enter city name");
+    const city = document.getElementById('location-input').value;
     const apiKey = '3021ea628941d0b00b4c4f176672ac88';
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
     fetch(url)
         .then(response => response.json()) // convert response data to JSON format
-        .then(data => { // log weather data
+        .then(data => {
+            // update weather-info element
+            const weatherInfo = document.getElementById('weather-info');
+            weatherInfo.innerHTML = `
+                <h2>Weather in ${city}</h2>
+                <p>Temperature: ${data.main.temp}°F</p>
+                <p>Description: ${data.weather[0].description}</p>
+            `;
+
+            // log weather data
             console.log(`Weather in ${city}:`);
             console.log(`Temperature: ${data.main.temp}°F`);
             console.log(`Description: ${data.weather[0].description}`);
         })
         .catch(error => {
             console.log('Error fetching weather data: ', error);
+            const weatherInfo = document.getElementById('weather-info');
+            weatherInfo.innerHTML = '<p>City note found</p>';
         })
 }
 
